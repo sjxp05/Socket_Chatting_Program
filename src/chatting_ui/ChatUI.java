5ea95d17e5180decfe2e71da59b0b18e88641421
j2iiin <jxxniie05@gmail.com>
2025-04-21 14:10:28 +0900

    ui separation 	new file:   src/chatting_ui/ChatUI.java 	modified:   src/chatting_ui/Main.java

---
+++ src/chatting_ui/ChatUI.java
@@ -0,0 +1,353 @@
+package chatting_ui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
+public class ChatUI extends JFrame {
+    JLabel roomName = new JLabel("새로운 채팅방");
+    JButton sendBt = new JButton("전송");
+    JButton exitBt = new JButton("나가기");
+
+    static JPanel msgPanel = new JPanel();
+    static JScrollPane scroll = new JScrollPane(msgPanel);
+    JTextArea textInput = new JTextArea();
+    JScrollPane textScroll = new JScrollPane(textInput);
+
+    JButton membersBt = new JButton("참여자");
+    JPanel membersPanel = new JPanel();
+    JScrollPane memScroll = new JScrollPane(membersPanel);
+    JButton nickChangeBt = new JButton("이름 변경");
+
+    private static SyncOnUpdate sync;
+
+    private volatile boolean shiftPressed = false;
+    private static volatile boolean viewMode = false;
+    private static volatile int nextMsgLocation = 10;
+
+    ChatUI() {
+        sync = this.new SyncOnUpdate();
+
+        setTitle("New Chat");
+        setSize(400, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        Container pane = getContentPane();
+        pane.setLayout(null);
+
+        roomName.setFont(new Font("Sans Serif", Font.BOLD, 15));
+        roomName.setHorizontalAlignment(JLabel.CENTER);
+        roomName.setBounds(92, 5, 200, 40);
+        pane.add(roomName);
+
+        exitBt.setFont(new Font("Sans Serif", Font.BOLD, 12));
+        exitBt.setBounds(10, 5, 70, 40);
+        exitBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        pane.add(exitBt);
+
+        membersBt.setFont(new Font("Sans Serif", Font.BOLD, 12));
+        membersBt.setBounds(307, 5, 70, 40);
+        membersBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sync.start("members");
+            }
+        });
+        pane.add(membersBt);
+
+        membersPanel.setBounds(0, 0, 400, 435);
+        membersPanel.setBackground(Color.LIGHT_GRAY);
+        membersPanel.setLayout(null);
+
+        memScroll.setBounds(10, 50, 368, 435);
+        memScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        memScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        pane.add(memScroll);
+        memScroll.setVisible(false);
+
+        nickChangeBt.setFont(new Font("Sans Serif", Font.BOLD, 10));
+        nickChangeBt.setBounds(260, 15, 80, 30);
+        nickChangeBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sync.start("change");
+            }
+        });
+
+        msgPanel.setBounds(0, 0, 350, 435);
+        msgPanel.setLayout(null);
+
+        scroll.setBounds(10, 50, 368, 435);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        pane.add(scroll);
+
+        textInput.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+        textInput.setBounds(0, 0, 290, 50);
+        textInput.setLineWrap(true);
+        textInput.requestFocus();
+        textInput.addKeyListener(new PressEnter());
+
+        textScroll.setBounds(10, 500, 290, 50);
+        textScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        textScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        pane.add(textScroll);
+
+        sendBt.setFont(new Font("Sans Serif", Font.BOLD, 15));
+        sendBt.setBounds(307, 500, 70, 50);
+        sendBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sync.start("send");
+            }
+        });
+        pane.add(sendBt);
+
+        setVisible(true);
+    }
+
+    class SyncOnUpdate implements Runnable {
+        private boolean flag = false;
+        String option = "";
+
+        SyncOnUpdate() {
+            Thread th = new Thread(this);
+            th.start();
+        }
+
+        synchronized void start(String optionReceived) {
+            flag = true;
+            option = optionReceived;
+            this.notify();
+        }
+
+        synchronized void waitForStart() {
+            if (!flag) {
+                try {
+                    this.wait();
+                } catch (Exception e) {
+                    System.exit(1);
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                waitForStart();
+                try {
+                    if (option.equals("send")) {
+
+                    } else if (option.equals("change")) {
+                        Main.changeNickname();
+                    } else if (option.equals("members")) {
+                        viewMembers();
+                    }
+                    flag = false;
+                } catch (Exception e) {
+                    System.exit(1);
+                }
+            }
+        }
+    }
+
+    class PressEnter implements KeyListener {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+                shiftPressed = true;
+            }
+
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                if (shiftPressed) {
+                    StringBuffer currentTxt = new StringBuffer(textInput.getText());
+
+                    SwingUtilities.invokeLater(() -> {
+                        textInput.setText(currentTxt + "\n");
+                        textInput.requestFocus();
+                    });
+                } else {
+                    sync.start("send");
+                }
+            }
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+                shiftPressed = false;
+            }
+        }
+    }
+
+    void viewMembers() {
+        if (viewMode) {
+            viewMode = false;
+            membersBt.setText("참여자");
+            membersPanel.removeAll();
+            memScroll.setVisible(false);
+        } else {
+            viewMode = true;
+            Main.viewRequest();
+            // while (true) {
+            // if (Main.added == true) {
+            // break;
+            // }
+            // } // 이거 넣어야되나?
+
+            int nextMemberLocation = 10;
+
+            membersBt.setText("채팅");
+
+            JLabel myLb = new JLabel("      " + Main.userName);
+            myLb.setFont(new Font("Sans Serif", Font.BOLD, 15));
+            myLb.setOpaque(true);
+            myLb.setBackground(new Color(245, 240, 190));
+            myLb.setBounds(0, nextMemberLocation, 400, 40);
+
+            membersPanel.add(myLb);
+            nextMemberLocation += 50;
+
+            membersPanel.add(nickChangeBt);
+            membersPanel.setComponentZOrder(nickChangeBt, 0);
+
+            for (String name : Main.nameList) {
+                JLabel nameLb = new JLabel("      " + name);
+                nameLb.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+                nameLb.setOpaque(true);
+                nameLb.setBackground(Color.WHITE);
+                nameLb.setBounds(0, nextMemberLocation, 400, 40);
+
+                membersPanel.add(nameLb);
+                nextMemberLocation += 50;
+            }
+
+            if (nextMemberLocation >= 435) {
+                membersPanel.setPreferredSize(new Dimension(400, nextMemberLocation));
+                membersPanel.revalidate();
+                repaint();
+            }
+
+            memScroll.setVisible(true);
+        }
+    }
+
+    void writeMessage() {
+        String msg = textInput.getText().strip();
+        SwingUtilities.invokeLater(() -> {
+            textInput.setText("");
+            textInput.requestFocus();
+        });
+
+        Main.sendMessage(msg);
+    }
+
+    static void refresh() {
+        if (viewMode) {
+            sync.start("members");
+            sync.start("members");
+        }
+    }
+
+    static void showNotices(String noticeMsg) {
+        JLabel noticeLb = new JLabel(noticeMsg);
+        noticeLb.setHorizontalAlignment(JLabel.CENTER);
+        noticeLb.setBounds(10, nextMsgLocation, 340, 20);
+        noticeLb.setFont(new Font("Sans Serif", Font.PLAIN, 13));
+        noticeLb.setForeground(Color.GRAY);
+        msgPanel.add(noticeLb);
+
+        Main.lastSpeakerID = -1;
+        nextMsgLocation += 25;
+
+        refresh();
+    }
+
+    static void showMessage(String sendName, String sendMsg, int sendID) {
+        if (viewMode) { // 채팅창 표시하기
+            sync.start("members");
+        }
+
+        int height = 20;
+        int lines = 1;
+
+        for (int i = 12; i < sendMsg.length(); i++) {
+            if (sendMsg.indexOf("<br>", i) >= 0) {
+                height += (21 + lines / 4);
+                lines++;
+                i = sendMsg.indexOf("<br>", i) + 4;
+            }
+        }
+
+        JLabel nameLb = new JLabel(sendName);
+        JLabel msgLb = new JLabel(sendMsg);
+
+        if (sendID == Main.userID) {
+            if (sendID == Main.lastSpeakerID) {
+                nextMsgLocation -= 10;
+            } else {
+                nameLb.setHorizontalAlignment(JLabel.RIGHT);
+                nameLb.setBounds(0, nextMsgLocation, 340, 20);
+                nameLb.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+                nameLb.setForeground(Color.GRAY);
+                msgPanel.add(nameLb);
+
+                Main.lastSpeakerID = sendID;
+                nextMsgLocation += 22;
+            }
+
+            msgLb.setHorizontalAlignment(JLabel.RIGHT);
+            msgLb.setBounds(0, nextMsgLocation, 340, height);
+            msgLb.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+            msgPanel.add(msgLb);
+            nextMsgLocation += (15 + height);
+
+        } else {
+            if (sendID == Main.lastSpeakerID) {
+                nextMsgLocation -= 10;
+            } else {
+                nameLb.setHorizontalAlignment(JLabel.LEFT);
+                nameLb.setBounds(10, nextMsgLocation, 340, 20);
+                nameLb.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+                nameLb.setForeground(Color.GRAY);
+                msgPanel.add(nameLb);
+
+                Main.lastSpeakerID = sendID;
+                nextMsgLocation += 22;
+            }
+
+            msgLb.setHorizontalAlignment(JLabel.LEFT);
+            msgLb.setBounds(10, nextMsgLocation, 340, height);
+            msgLb.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+            msgPanel.add(msgLb);
+            nextMsgLocation += (15 + height);
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            if (nextMsgLocation >= 435) {
+                msgPanel.setPreferredSize(new Dimension(350, nextMsgLocation));
+                msgPanel.revalidate();
+            }
+            msgPanel.repaint();
+
+            SwingUtilities.invokeLater(() -> {
+                scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getMaximum());
+            });
+            return;
+        });
+    }
+}
--- src/chatting_ui/Main.java
+++ src/chatting_ui/Main.java
@@ -4,209 +4,26 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.*;
 
-public class Main extends JFrame {
-    JLabel roomName = new JLabel("새로운 채팅방");
-    JPanel msgPanel = new JPanel();
-    JScrollPane scroll = new JScrollPane(msgPanel);
-    JTextArea textInput = new JTextArea();
-    JScrollPane textScroll = new JScrollPane(textInput);
-    JButton sendBt = new JButton("전송");
-    JButton exitBt = new JButton("나가기");
-
-    JButton membersBt = new JButton("참여자");
-    JPanel membersPanel = new JPanel();
-    JScrollPane memScroll = new JScrollPane(membersPanel);
-    JButton nickChangeBt = new JButton("이름 변경");
-
-    private SyncOnUpdate sync = new SyncOnUpdate();
-    private ArrayList<String> nameList = new ArrayList<>();
-
+public class Main {
     private static Socket socket = null;
     private static BufferedReader in;
     private static PrintWriter out;
 
-    private String userHome = System.getProperty("user.home");
-    private File saveFile = new File(userHome, "chat-client-info.txt");
+    private static String userHome = System.getProperty("user.home");
+    private static File saveFile = new File(userHome, "chat-client-info.txt");
 
-    private volatile int userID;
-    private volatile String userName = "";
-    private volatile int lastSpeakerID = -1;
-    private volatile int nextMsgLocation = 10;
-    private volatile boolean shiftPressed = false;
+    static ArrayList<String> nameList = new ArrayList<>();
 
-    private volatile boolean viewMode = false;
-    private volatile boolean added = false;
+    static int userID;
+    static volatile String userName = "";
+    static volatile int lastSpeakerID = -1;
+    static volatile boolean added = false;
 
     private Main() {
-        setTitle("New Chat");
-        setSize(400, 600);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        Container pane = getContentPane();
-        pane.setLayout(null);
-
-        roomName.setFont(new Font("Sans Serif", Font.BOLD, 15));
-        roomName.setHorizontalAlignment(JLabel.CENTER);
-        roomName.setBounds(92, 5, 200, 40);
-        pane.add(roomName);
-
-        exitBt.setFont(new Font("Sans Serif", Font.BOLD, 12));
-        exitBt.setBounds(10, 5, 70, 40);
-        exitBt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        pane.add(exitBt);
-
-        membersBt.setFont(new Font("Sans Serif", Font.BOLD, 12));
-        membersBt.setBounds(307, 5, 70, 40);
-        membersBt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sync.start("members");
-            }
-        });
-        pane.add(membersBt);
-
-        membersPanel.setBounds(0, 0, 400, 435);
-        membersPanel.setBackground(Color.LIGHT_GRAY);
-        membersPanel.setLayout(null);
-
-        memScroll.setBounds(10, 50, 368, 435);
-        memScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        memScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        pane.add(memScroll);
-        memScroll.setVisible(false);
-
-        nickChangeBt.setFont(new Font("Sans Serif", Font.BOLD, 10));
-        nickChangeBt.setBounds(260, 15, 80, 30);
-        nickChangeBt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sync.start("change");
-            }
-        });
-
-        msgPanel.setBounds(0, 0, 350, 435);
-        msgPanel.setLayout(null);
-
-        scroll.setBounds(10, 50, 368, 435);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        pane.add(scroll);
-
-        textInput.setFont(new Font("Sans Serif", Font.PLAIN, 15));
-        textInput.setBounds(0, 0, 290, 50);
-        textInput.setLineWrap(true);
-        textInput.requestFocus();
-        textInput.addKeyListener(new PressEnter());
-
-        textScroll.setBounds(10, 500, 290, 50);
-        textScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        textScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        pane.add(textScroll);
-
-        sendBt.setFont(new Font("Sans Serif", Font.BOLD, 15));
-        sendBt.setBounds(307, 500, 70, 50);
-        sendBt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sync.start("send");
-            }
-        });
-        pane.add(sendBt);
-
-        setVisible(true);
+        new ChatUI();
         setNickname();
-    }
-
-    class SyncOnUpdate implements Runnable {
-        boolean flag = false;
-        String option;
-
-        SyncOnUpdate() {
-            Thread thread = new Thread(this);
-            thread.start();
-        }
-
-        synchronized void start(String optionReceived) {
-            flag = true;
-            option = optionReceived;
-            this.notify();
-        }
-
-        synchronized void waitForStart() {
-            if (!flag) {
-                try {
-                    this.wait();
-                } catch (Exception e) {
-                    System.exit(1);
-                }
-            }
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                waitForStart();
-                try {
-                    if (option.equals("send")) {
-                        sendMessage();
-                    } else if (option.equals("change")) {
-                        changeNickname();
-                    } else if (option.equals("members")) {
-                        viewMembers();
-                    }
-                    flag = false;
-                } catch (Exception e) {
-                    System.exit(1);
-                }
-            }
-        }
-    }
-
-    class PressEnter implements KeyListener {
-        @Override
-        public void keyPressed(KeyEvent e) {
-            if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-                shiftPressed = true;
-            }
-
-            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                if (shiftPressed) {
-                    StringBuffer currentTxt = new StringBuffer(textInput.getText());
-
-                    SwingUtilities.invokeLater(() -> {
-                        textInput.setText(currentTxt + "\n");
-                        textInput.requestFocus();
-                    });
-                } else {
-                    sync.start("send");
-                }
-            }
-        }
-
-        @Override
-        public void keyTyped(KeyEvent e) {
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-                shiftPressed = false;
-            }
-        }
     }
 
     void setNickname() {
@@ -273,7 +90,7 @@ public class Main extends JFrame {
         }
     }
 
-    void changeNickname() {
+    static void changeNickname() {
         String nickNameMsg = "변경할 닉네임을 입력해 주세요.";
 
         ChangeNickname: while (true) {
@@ -315,70 +132,20 @@ public class Main extends JFrame {
         }
     }
 
-    void viewMembers() {
-        if (viewMode) {
-            viewMode = false;
-            membersBt.setText("참여자");
-            membersPanel.removeAll();
-            memScroll.setVisible(false);
-        } else {
-            viewMode = true;
-            nameList.clear();
+    static void viewRequest() {
+        nameList.clear();
 
-            out.println("@viewNickname@");
-            while (true) {
-                if (added == true) {
-                    nameList.sort(null);
-                    added = false;
-                    break;
-                }
+        out.println("@viewNickname@");
+        while (true) {
+            if (added == true) {
+                nameList.sort(null);
+                added = false;
+                break;
             }
-
-            int nextMemberLocation = 10;
-
-            membersBt.setText("채팅");
-
-            JLabel myLb = new JLabel("      " + userName);
-            myLb.setFont(new Font("Sans Serif", Font.BOLD, 15));
-            myLb.setOpaque(true);
-            myLb.setBackground(new Color(245, 240, 190));
-            myLb.setBounds(0, nextMemberLocation, 400, 40);
-
-            membersPanel.add(myLb);
-            nextMemberLocation += 50;
-
-            membersPanel.add(nickChangeBt);
-            membersPanel.setComponentZOrder(nickChangeBt, 0);
-
-            for (String name : nameList) {
-                JLabel nameLb = new JLabel("      " + name);
-                nameLb.setFont(new Font("Sans Serif", Font.PLAIN, 15));
-                nameLb.setOpaque(true);
-                nameLb.setBackground(Color.WHITE);
-                nameLb.setBounds(0, nextMemberLocation, 400, 40);
-
-                membersPanel.add(nameLb);
-                nextMemberLocation += 50;
-            }
-
-            if (nextMemberLocation >= 435) {
-                membersPanel.setPreferredSize(new Dimension(400, nextMemberLocation));
-                membersPanel.revalidate();
-                repaint();
-            }
-
-            memScroll.setVisible(true);
         }
     }
 
-    void sendMessage() {
-        String msg = textInput.getText().strip();
-
-        SwingUtilities.invokeLater(() -> {
-            textInput.setText("");
-            textInput.requestFocus();
-        });
-
+    static void sendMessage(String msg) {
         if (msg.length() == 0) {
             return;
         }
@@ -474,110 +241,25 @@ public class Main extends JFrame {
         } else if (input.indexOf("@changed@") >= 0) {
             int changedID = Integer.parseInt(input.substring(0, input.indexOf("@")));
 
-            if (viewMode) { // 새로고침
-                viewMembers();
-                sync.start("members");
-            }
-
             if (changedID == lastSpeakerID) {
                 lastSpeakerID = -1;
             }
+
+            ChatUI.refresh();
             return;
         }
 
         if (input.indexOf(';') == -1) {
             if (input.indexOf('@') == 0) {
-                if (viewMode) { // 새로고침
-                    viewMembers();
-                    sync.start("members");
-                }
-
-                JLabel noticeLb = new JLabel(input.substring(1));
-                noticeLb.setHorizontalAlignment(JLabel.CENTER);
-                noticeLb.setBounds(10, nextMsgLocation, 340, 20);
-                noticeLb.setFont(new Font("Sans Serif", Font.PLAIN, 13));
-                noticeLb.setForeground(Color.GRAY);
-                msgPanel.add(noticeLb);
-
-                lastSpeakerID = -1;
-                nextMsgLocation += 25;
+                ChatUI.showNotices(input.substring(1));
             }
         } else {
-            if (viewMode) { // 채팅창 표시하기
-                viewMembers();
-            }
-
             String sendName = input.substring(0, input.indexOf(';'));
             String sendMsg = input.substring(input.indexOf(';') + 1, input.lastIndexOf(';'));
             int sendID = Integer.parseInt(input.substring(input.lastIndexOf(';') + 1));
-            int height = 20;
-            int lines = 1;
 
-            for (int i = 12; i < sendMsg.length(); i++) {
-                if (sendMsg.indexOf("<br>", i) >= 0) {
-                    height += (21 + lines / 4);
-                    lines++;
-                    i = sendMsg.indexOf("<br>", i) + 4;
-                }
-            }
-
-            JLabel nameLb = new JLabel(sendName);
-            JLabel msgLb = new JLabel(sendMsg);
-
-            if (sendID == userID) {
-                if (sendID == lastSpeakerID) {
-                    nextMsgLocation -= 10;
-                } else {
-                    nameLb.setHorizontalAlignment(JLabel.RIGHT);
-                    nameLb.setBounds(0, nextMsgLocation, 340, 20);
-                    nameLb.setFont(new Font("Sans Serif", Font.PLAIN, 14));
-                    nameLb.setForeground(Color.GRAY);
-                    msgPanel.add(nameLb);
-
-                    lastSpeakerID = sendID;
-                    nextMsgLocation += 22;
-                }
-
-                msgLb.setHorizontalAlignment(JLabel.RIGHT);
-                msgLb.setBounds(0, nextMsgLocation, 340, height);
-                msgLb.setFont(new Font("Sans Serif", Font.PLAIN, 15));
-                msgPanel.add(msgLb);
-                nextMsgLocation += (15 + height);
-
-            } else {
-                if (sendID == lastSpeakerID) {
-                    nextMsgLocation -= 10;
-                } else {
-                    nameLb.setHorizontalAlignment(JLabel.LEFT);
-                    nameLb.setBounds(10, nextMsgLocation, 340, 20);
-                    nameLb.setFont(new Font("Sans Serif", Font.PLAIN, 14));
-                    nameLb.setForeground(Color.GRAY);
-                    msgPanel.add(nameLb);
-
-                    lastSpeakerID = sendID;
-                    nextMsgLocation += 22;
-                }
-
-                msgLb.setHorizontalAlignment(JLabel.LEFT);
-                msgLb.setBounds(10, nextMsgLocation, 340, height);
-                msgLb.setFont(new Font("Sans Serif", Font.PLAIN, 15));
-                msgPanel.add(msgLb);
-                nextMsgLocation += (15 + height);
-            }
+            ChatUI.showMessage(sendName, sendMsg, sendID);
         }
-
-        SwingUtilities.invokeLater(() -> {
-            if (nextMsgLocation >= 435) {
-                msgPanel.setPreferredSize(new Dimension(350, nextMsgLocation));
-                msgPanel.revalidate();
-            }
-            repaint();
-
-            SwingUtilities.invokeLater(() -> {
-                scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getMaximum());
-            });
-            return;
-        });
     }
 
     public static void main(String[] args) {
