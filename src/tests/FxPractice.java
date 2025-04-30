0837c5fe1d7a98f90bc64e147d6f8f9f4c8de40e
j2iiin <jxxniie05@gmail.com>
2025-04-30 12:31:20 +0900

    new file:   src/tests/FxPractice.java

---
+++ src/tests/FxPractice.java
@@ -0,0 +1,17 @@
+package tests;
+
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+public class FxPractice extends Application {
+    @Override
+    public void start(Stage arg0) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
