d707b4f532a2b0899311f2829c5530ac51a7c9d4
j2iiin <jxxniie05@gmail.com>
2025-05-05 11:24:13 +0900

    zzajal modified - no function changes 	modified:   src/chatting_ui/client/ChatUI.java 	modified:   src/chatting_ui/client/Main.java

--- src/chatting_ui/client/Main.java
+++ src/chatting_ui/client/Main.java
@@ -320,7 +320,19 @@ public class Main {
             String sendMsg = input.substring(input.indexOf(';') + 1, input.lastIndexOf(';'));
             int sendID = Integer.parseInt(input.substring(input.lastIndexOf(';') + 1));
 
-            ui.showMessage(sendName, sendMsg, sendID); // 채팅창에 사용자이름+메시지 띄우기
+            // 메시지의 줄 개수를 세서 라벨의 세로 길이 설정
+            int height = 20;
+            int lines = 1;
+
+            for (int i = 12; i < sendMsg.length(); i++) {
+                if (sendMsg.indexOf("<br>", i) >= 0) {
+                    height += (21 + lines / 4);
+                    lines++;
+                    i = sendMsg.indexOf("<br>", i) + 4;
+                }
+            }
+
+            ui.showMessage(sendName, sendMsg, sendID, height); // 채팅창에 사용자이름+메시지 띄우기
         }
     }
 
@@ -338,6 +350,7 @@ public class Main {
             }
         } catch (Exception e) { // 서버 연결이 끊어졌을 경우
             JOptionPane.showMessageDialog(null, "서버 연결에 실패했습니다 ㅠㅠ\n황지인에게 서버를 열어달라고 요청해보세요!");
+            System.exit(1);
         }
     }
 }
