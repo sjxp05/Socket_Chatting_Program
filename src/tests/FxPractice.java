c529a319c5e224435de0dd2b8601515887f73a4a
j2iiin <jxxniie05@gmail.com>
2025-05-05 12:22:39 +0900

    found a better way than javafx !!!!! 	modified:   src/tests/FxPractice.java 	modified:   src/tests/fxWindow.fxml

--- src/tests/FxPractice.java
+++ src/tests/FxPractice.java
@@ -3,15 +3,31 @@ package tests;
 import javafx.application.*;
 import javafx.fxml.*;
 import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.text.*;
 import javafx.stage.*;
+import javafx.event.*;
 
 public class FxPractice extends Application {
+    @FXML
+    private Label myLabel;
+
+    @FXML
+    private Button myButton;
+
+    @FXML // 이거였누....
+    private void buttonClick(ActionEvent event) {
+        myLabel.setFont(new Font("Segoe UI Emoji", 20));
+        myLabel.setText("this text has changed 🌟");
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(this.getClass().getResource("fxWindow.fxml"));
         stage.setTitle("Test Program");
         stage.setScene(new Scene(root));
         // or stage.setScene(new Scene(root, 300, 200));
+
         stage.show();
     }
 
